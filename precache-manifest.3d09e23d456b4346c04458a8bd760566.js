7138fcfa5a8619ae2b23342b18ddcf4e48dd2c37
David Endersby <davidendersby@Davids-MacBook-Pro.local>
2020-09-10 21:21:36 +0100

    Updates

---
+++ precache-manifest.3d09e23d456b4346c04458a8bd760566.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4de8025192825af112dae341fa8f7b8",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "6dd787f6e630da6f8d45",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc49f4c1c56e4f18a8ee",
+    "url": "/my-app/static/js/2.ce45701b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.ce45701b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd787f6e630da6f8d45",
+    "url": "/my-app/static/js/main.e2a065e8.chunk.js"
+  },
+  {
+    "revision": "631d6da5ed21fbcb2c56",
+    "url": "/my-app/static/js/runtime-main.0669e9ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
